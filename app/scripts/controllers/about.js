66912248c7b8365a50055a5b0451a022686fc3f1
Joshua Raichur <hello@joshuaraichur.com>
2015-07-19 01:57:05 +0530

    Add: Angular, Yo!

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vidConfApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vidConfApp
+ */
+angular.module('vidConfApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
