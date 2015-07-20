3e6d5bfbf7dbcdd7d905fa9c0d1d5f532cb506fe
Joshua Raichur <hello@joshuaraichur.com>
2015-07-20 11:00:11 +0530

    Add: Angular, Yo!

---
+++ public/app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicApp
+ * @description
+ * # publicApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
